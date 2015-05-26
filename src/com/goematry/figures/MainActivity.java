6036ba49e8a705e685cf9bd4b6c6cc3fc06929e1
Mohit <mohit110891@gmail.com>
2015-05-26 16:51:44 +0530

    First

---
+++ src/com/goematry/figures/MainActivity.java
@@ -0,0 +1,23 @@
+package com.goematry.figures;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * 
+ * 
+ * @author Mohit Soni
+ *
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		
+	}
+
+
+}
